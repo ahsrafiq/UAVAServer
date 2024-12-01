1f953c86458f9cbc26ba7c0b66d2f694177188d5
Ahsan Rafiq <ahsaanrafiq8@gmail.com>
2024-12-01 23:46:19 +0500

    Update JWT Middleware

--- config/jwtMiddleware.js
+++ config/jwtMiddleware.js
@@ -1,22 +1,18 @@
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
 
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split(' ')[1];
-  
-
+const authenticateToken = (req, res, next) => {
+  const token = req.header('Authorization')?.replace('Bearer ', ''); // Assumes token is passed as Bearer token in the Authorization header
   if (!token) {
-    return res.status(401).json({ success: false, message: 'Unauthorized' });
+    return res.status(401).json({ success: false, message: 'Authorization token required' });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
-      return res.status(403).json({ success: false, message: 'Invalid token' });
+      return res.status(403).json({ success: false, message: 'Invalid or expired token' });
     }
     req.user = user;
     next();
   });
-}
+};
 
 module.exports = authenticateToken;
